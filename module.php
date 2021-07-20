3d3dd4dd1d728cab76065f98de39d2e351026aba
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2021-07-20 12:06:35 -0700

    This is a test commit.

--- module.php
+++ module.php
@@ -7,6 +7,8 @@ class DirectoryModule extends Module {
         parent::__construct();
     }
 
+    // This is a test comment that is being written for a test commit.
+    
     public function showMemberDirectory(){
 
         $params = $_POST;
