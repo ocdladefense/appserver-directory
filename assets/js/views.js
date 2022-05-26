ff4502620ce42c5b4e27b892b3e600b89e0939d5
Jose Bernal <jbernal.web.dev@gmail.com>
2022-05-25 23:30:37 -0700

    Be able to switch views.

---
+++ assets/js/views.js
@@ -0,0 +1,51 @@
+import views from "/modules/directory/assets/js/directory.js";
+console.log(views);
+/**
+ * To test this code execute either:
+ * switchView("list");
+ * switchView("map");
+ */
+const viewCache = {}; 
+
+let currentView = "list";
+
+function loadView(name) {
+  return views[name].init();
+}
+
+function updateView(newNode) {
+
+	let container = document.getElementById("view");
+	let current = container.cloneNode();
+	let parent = container.parentNode;
+
+	current.appendChild(newNode);
+
+	return parent.replaceChild(current,container);
+}
+
+async function switchView(name) {
+	let module, newNode, oldNode;
+	if(null == viewCache[name]) {
+    	newNode = loadView(name);
+	} else {
+		newNode = viewCache[name];
+	}
+
+    oldNode = updateView(newNode);
+
+	// Place the old domtree in the cache,
+	// for later use, i.e., when we 
+	// switch back to list view.
+	viewCache[currentView] = oldNode;
+	currentView = name;
+	// Post-render operations.
+	// For example, execute any init
+	// function associated with the newly-loaded module.
+	// qb.render("custom");
+    views[name].render();
+}
+
+window.switchView = switchView;
+
+
