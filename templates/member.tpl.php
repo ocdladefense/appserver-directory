3b658eb55f53db064516a7d87dce9cedb1de8c8d
Jose Bernal <jbernal.web.dev@gmail.com>
2022-05-29 21:23:27 -0700

    Simplify layout of members and experts.

--- templates/member.tpl.php
+++ templates/member.tpl.php
@@ -10,18 +10,21 @@ img.obj {
     margin: 10px;
     border-radius: 5px;
 }
+
+.record-documents {
+    margin-top: 25px;
+}
 </style>
 
 
-<a class="back-link" href="/directory/members" style="float: left;"><i class="fa fa-arrow-left" style="font-size:25px;color:blue"></i></a><br /><br />
 
 <div class="search">
     <?php print $search; ?>
 </div>
 
-<div class="directory-container">
 
-<h1 style="text-align:center; margin-bottom:10px;">OCDLA Member</h1>
+
+
 
 <?php if($user->isAdmin()) : ?>
     <div>
@@ -29,54 +32,83 @@ img.obj {
     </div>
 <?php endif; ?>
 
-<br />
-<br />
 
 <?php if(empty($contacts)) : ?>
-    <h1 style="text-align:center;">Couldn't find anyone using those search parameters......</h1>
+    <h1 style="text-align:center;">Couldn't find anyone using those search parameters...</h1>
 <?php endif; ?>
 
 <?php $isSingle ? $singleClass = "is-single" : ""; ?>
 
-<div class="directory-list">
 
-    <div class="file-upload" style="border:1px solid #ccc; padding:50px; height:300px;">
-        <i class="fa-solid fa-cloud-arrow-up fa-2x" style="font-size:4.0em; color:blue;"></i>
-        <input type="file" id="upload" multiple />
-        <div id="preview"></div>
+
+
+
+
+<?php foreach($contacts as $c): ?>
+
+    <div class="list-item <?php print $singleClass; ?>" data-contactId="<?php print $c->getId(); ?>">
+        <h1>
+            <?php if(!empty($c->getFirstName())): ?>
+                <?php print $c->getFirstName() . " " . $c->getLastName(); ?>
+            <?php endif; ?>
+        </h1>
+
+        <p class="secondary">
+            <?php !empty($c->getOccupationFieldType()) ? print $c->getOccupationFieldType() : print "<br />"; ?>
+        </p>
+        
+        <p>
+            <?php !empty($c->getOcdlaOrganization()) ? print $c->getOcdlaOrganization() : print "<br />"; ?>
+        </p>
+
+        <a href="tel:<?php print $c->getPhoneNumericOnly(); ?>">
+            <?php print $c->getPhone(); ?>
+        </a>
+
+        <p>
+            <?php !empty($c->getMailingCity()) ? print $c->getMailingCity() . ", " . $c->getMailingState() : print "City Not Listed"; ?>
+        </p>
+        
+        <?php !empty($c->getEmail()) ? print "<a href='mailto: {$c->getEmail()}' style='text-decoration:none;'>{$c->getEmail()}</a>" : print "No Email Available"; ?>
+
+        
+        <?php if(!empty($c->getAreasOfInterest())) : ?>
+            <p>
+                <strong>
+                    Areas of Interest:
+                </strong>
+            </p>
+            
+            <p><?php print $c->getAreasOfInterest(); ?></p>
+        <?php endif; ?>
+
     </div>
 
-    <?php foreach($contacts as $c) : ?>
+<?php endforeach; ?>
 
-        <div class="list-item <?php print $singleClass; ?>" data-contactId="<?php print $c->getId(); ?>">
-            <p class="primary"><?php !empty($c->getFirstName()) ? print $c->getFirstName() . " " . $c->getLastName() : print "<br />";  ?></p>
-            <p class="secondary"><?php !empty($c->getOccupationFieldType()) ? print $c->getOccupationFieldType() : print "<br />"; ?></p>
-            <p><?php !empty($c->getOcdlaOrganization()) ? print $c->getOcdlaOrganization() : print "<br />"; ?></p>
-            <a href="tel:<?php print $c->getPhoneNumericOnly(); ?>"><?php print $c->getPhone(); ?></a>
-            <p><?php !empty($c->getMailingCity()) ? print $c->getMailingCity() . ", " . $c->getMailingState() : print "City Not Listed"; ?></p>
-            <?php !empty($c->getEmail()) ? print "<a href='mailto: {$c->getEmail()}' style='text-decoration:none;'>{$c->getEmail()}</a>" : print "No Email Available"; ?>
-
-            <br /><br />
+<div class="record-documents">
+    <form id="contact-uploads" method="post" enctype="multipart/form-data" action="https://appdev.ocdla.org/file/upload">
+        <h2>Related documents</h2>
             
-            <?php if(!empty($c->getAreasOfInterest())) : ?>
-                <p>
-                    <strong>
-                        Areas of Interest:
-                    </strong>
-                </p>
-                
-                <p><?php print $c->getAreasOfInterest(); ?></p>
-            <?php endif; ?>
+        <div class="file-upload" style="border:1px solid #ccc; padding:50px;">
+            <i class="fa-solid fa-cloud-arrow-up fa-2x" style="font-size:4.0em; color:blue;"></i>
+            <input name="thefiles[]" type="file" id="upload" multiple />
         </div>
 
-    <?php endforeach; ?>
+        <div class="form-item">
+            <input type="submit" value="Upload" />
+        </div>
 
+        <div id="preview"></div>
+
+    </form>
 </div>
 
-</div>
+
+
 
 <!--  NOTE: this could be used to display a map locating the member's business address. 
     <script type="module" src="<?php print module_path(); ?>/assets/js/directory.js">
 </script> -->
-<script type="module" src="<?php print module_path(); ?>/assets/js/upload.js">
+<script src="/node_modules/@ocdladefense/node-file-upload/upload.js">
 </script>
