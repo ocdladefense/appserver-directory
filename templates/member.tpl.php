472963ff56c777cdd4e2bd75ffa6fe85b8a382c2
Ubuntu <ubuntu@ip-172-31-58-171.us-west-2.compute.internal>
2022-11-14 04:44:10 +0000

    New member templates.

--- templates/member.tpl.php
+++ templates/member.tpl.php
@@ -1,21 +1,52 @@
-<link rel="stylesheet" type="text/css" href="<?php print module_path(); ?>/assets/css/directory.css"></link>
+<?php
+
+
+$aoi = $c->getAreasOfInterest();
+
+
+?>
+
+
+<link rel="stylesheet" type="text/css" href="<?php print module_path(); ?>/assets/css/directory.css" />
 
 
 
 <style type="text/css">
-img.obj {
-    vertical-align: middle;
-    padding: 15px;
-    border: 1px solid #eee;
-    margin: 10px;
-    border-radius: 5px;
-}
+    img.obj {
+        vertical-align: middle;
+        padding: 15px;
+        border: 1px solid #eee;
+        margin: 10px;
+        border-radius: 5px;
+    }
 
-.record-documents {
-    margin-top: 25px;
-}
+    .record-documents {
+        margin-top: 25px;
+    }
+
+    h2.section-title {
+        border-radius: 9px;
+        width: 25%;
+        padding: 5px;
+        padding-left: 0px;
+        font-size: 1.0rem;
+        margin-top: 30px;
+    }
+
+    .member-status {
+        background-color: rgba(85,99,141,1.0);
+        color: #fff;
+        border-radius: 3px;
+        padding: 3px;
+        display: inline-block;
+    }
+
+    .directory-title {
+        margin-top: 25px;
+    }
+
+
 </style>
-
 
 
 <div class="search">
@@ -28,12 +59,12 @@ img.obj {
 
 <?php if($user->isAdmin()) : ?>
     <div>
-        <p><?php print $query; ?></p>
+        <?php print $query; ?>
     </div>
 <?php endif; ?>
 
 
-<?php if(empty($c)) : ?>
+<?php if(empty($c)): ?>
     <h1 style="text-align:center;">We couldn't find that member...</h1>
 <?php endif; ?>
 
@@ -42,45 +73,141 @@ img.obj {
 
 
 
+<div class="breadcrumb">
+    <a href="/directory/members">Directory</a> // <a href="/directory/members">OCDLA Members</a> // <?php print $c->FirstName . " " . $c->LastName; ?>
+</div>
 
 
 
-
-<div class="list-item <?php print $singleClass; ?>" data-contactId="<?php print $c->getId(); ?>">
-    <h1>
-        <?php if(!empty($c->getFirstName())): ?>
-            <?php print $c->getFirstName() . " " . $c->getLastName(); ?>
-        <?php endif; ?>
-    </h1>
-
-    <p class="secondary">
-        <?php !empty($c->getOccupationFieldType()) ? print $c->getOccupationFieldType() : print "<br />"; ?>
-    </p>
-    
-    <p>
-        <?php !empty($c->getOcdlaOrganization()) ? print $c->getOcdlaOrganization() : print "<br />"; ?>
-    </p>
-
-    <a href="tel:<?php print $c->getPhoneNumericOnly(); ?>">
-        <?php print $c->getPhone(); ?>
-    </a>
-
-    <p>
-        <?php !empty($c->getMailingCity()) ? print $c->getMailingCity() . ", " . $c->getMailingState() : print "City Not Listed"; ?>
-    </p>
-
-    <?php !empty($c->getEmail()) ? print "<a href='mailto: {$c->getEmail()}' style='text-decoration:none;'>{$c->getEmail()}</a>" : print "No Email Available"; ?>
+<div class="list-item <?php print $singleClass; ?>" data-contact-id="<?php print $c->getId(); ?>">
 
     
-    <?php if(!empty($c->getAreasOfInterest())) : ?>
+    <div class="directory-title">
+        <div class="member-status">OCDLA Member</div>
+        <h1 class="directory-name">
+            <?php print $c->FirstName . " " . $c->LastName; ?>
+        </h1>
+    </div>
+
+    
+    <?php if(!empty($c->Ocdla_Organization__c)): ?>
         <p>
-            <strong>
-                Areas of Interest:
-            </strong>
+            <?php print $c->Ocdla_Organization__c; ?>
         </p>
-        
-        <p><?php print $c->getAreasOfInterest(); ?></p>
     <?php endif; ?>
+    
+
+
+    <section>
+        <h2 class="section-title">Occupation</h2>
+        <?php if(!empty($c->Ocdla_Occupation_Field_Type__c)): ?>
+            <p class="secondary">
+                <?php print $c->Ocdla_Occupation_Field_Type__c; ?>
+            </p>
+        <?php endif; ?>
+        <?php if(!empty($c->Ocdla_Bar_Number__c)): ?>
+            <div class="row">
+                <div class="text cell cell-label">
+                    OSB #<?php print $c->Ocdla_Bar_Number__c; ?>
+                </div>
+            </div>
+        <?php endif; ?>
+    </section>
+
+
+
+
+
+
+    <?php if(!empty($c->Ocdla_Investigator_License_Number__c)): ?>
+        <div class="row">
+            <div class="text cell cell-label">
+                Investigator #<?php print $c->Ocdla_Investigator_License_Number__c; ?>
+            </div>
+        </div>
+    <?php endif; ?>
+
+
+    
+    <section>
+        <h2 class="section-title">Location</h2>
+        <?php if(!empty($c->MailingCity)): ?>
+            <p>
+                <?php print $c->MailingCity . ", " . $c->MailingState; ?>
+            </p>
+        <?php endif; ?>
+    </section>
+
+
+    <section>
+        <h2 class="section-title">Contact</h2>
+        Phone: <a href="tel:<?php print $c->getPhoneNumericOnly(); ?>">
+            <?php print $c->Phone; ?>
+        </a>
+
+        <br />
+        <?php
+         
+/*
+         <div class="row">
+         <div class="text cell cell-label">
+             Phone:
+         </div>
+         <div class="text cell">
+             {# phoneNumberFormat( work_phone ) #}
+             {% if contact.Ocdla_Publish_Work_Phone__c %}
+                 {{ contact.OrderApi__Work_Phone__c }}
+             {% else %}
+                 This member has chosen not to publish their phone number.
+             {% endif %}
+         </div>
+ 
+     </div>
+     <div class="row">
+         <div class="text cell cell-label">
+             Cell Phone:
+         </div>
+         <div class="text cell" width="76%">
+             {# phoneNumberFormat(cell_phone) #}
+             {% if contact.Ocdla_Publish_Work_Phone__c %}
+                 {{ contact.Ocdla_Cell_Phone__c }}
+             {% else %}
+                 This member has chosen not to publish their cell phone.
+             {% endif %}
+         </div>
+     </div>
+     <div class="row">
+         <div class="text cell cell-label">
+             <font color="000000">Fax:</font>
+         </div>
+         <div class="text cell">
+             {# phoneNumberFormat(fax_number) #}
+             {{ contact.Fax }}
+         </div>
+     </div>
+
+*/
+
+        ?>
+
+        <?php if(!empty($c->Email)): ?>
+            Email: <a href='mailto: <?php print $c->Email; ?>' style='text-decoration:none;'>
+                <?php print $c->Email; ?>
+            </a>
+        <?php endif; ?>
+    </section>
+
+
+
+    <section>
+        <h2 class="section-title">Areas of Interest</h2>
+
+        <?php if($c->hasInterests()): ?>
+            <p>
+                <?php print $c->getAreasOfInterest(); ?>
+            </p>
+        <?php endif; ?>
+    </section>
 
 </div>
 
