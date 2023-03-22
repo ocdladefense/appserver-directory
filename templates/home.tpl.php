eb9d62f536c6ab22019c6fae8eb884ca0b0e2cd8
Ubuntu <ubuntu@ip-172-31-58-171.us-west-2.compute.internal>
2023-03-22 15:39:34 +0000

    Format 2022 membership directory landing page.

--- templates/home.tpl.php
+++ templates/home.tpl.php
@@ -1,7 +1,16 @@
 
 
 
+<style type="text/css">
+	h1 {
+		margin-bottom: 15px;
+	}
 
+	h2 {
+		margin-top: 25px;
+		margin-bottom: 10px;
+	}
+</style>
 
 <!--
 <div class="crumbs">
@@ -10,103 +19,90 @@
 </div>
 -->
 			
-<h1>OCDLA Directory Search</h1>
+<h1>OCDLA Membership Directory Search</h1>
 
-<a href="/directory/members">Membership Directory</a>
-<br />
-<a href="/directory/experts">Expert Witness Directory</a>
-
-
-
-
-
-<!-- <a href="javascript: history.go(-1);">Back to Previous Page</a> -->
-
-
-
-<!--
-<div class="search-box">
-	<h2>
-		Expert Witness Directory
-	</h2>
-	<div class="search-item">
-		<a href="/directory/expert-witness/search">Search the Directory</a>
-	</div>
-</div>
--->
+<h2>Membership Directory Links</h2>
+<ul>
+	<li>
+		<a href="/directory/members">Membership Directory / Database</a>
+	</li>
+	<li>
+		<a href="/directory/experts">Expert Witness Directory / Database</a>
+	</li>
+</ul>
 
 
 
-<div class="search-box">
-	<h2>Membership Directory Section PDFs, Updated Daily</h2>
-	<div class="search-item">
+<h2>Membership Directory Section PDFs, Updated Daily</h2>
+
+<ul>
+	<li>
 		<a href="/content/uploads/modules/directory/current_members_alpha.pdf">Last Name</a>
-	</div>
-	<div class="search-item">
+	</li>
+	<li>
 		<a href="/content/uploads/modules/directory/current_members_city.pdf">City</a>
-	</div>
-	<div class="search-item">
+	</li>
+	<li>
 		<a href="/content/uploads/modules/directory/current_members_interests.pdf">Members Interests</a>
-	</div>
-	<div class="search-item">
+	</li>
+	<li>
 		<a href="/content/uploads/modules/directory/current_members_nonlawyer.pdf">Nonlawyer Members</a>
-	</div>
-	<div class="search-item">
+	</li>
+	<li>
 		<a href="/content/uploads/modules/directory/current_contracts.pdf">Public Defense Contracts</a>
-	</div>
-</div>
+	</li>
+</ul>
 
 
-
+  
+<h2>Complete 2022 Membership Directory PDF</h2>
+<a href="/content/uploads/modules/directory/OCDLA_2022_Membership_Directory.pdf">
+	<img src="/content/uploads/modules/directory/directory.jpg" />
+</a>
+	
 <div class="search-box">
-	<a href="/content/uploads/modules/directory/OCDLA_2021_Membership_Directory.pdf">
-		<img src="/content/uploads/modules/directory/directory.jpg" />
-	</a>
-	<h2>Complete Membership Directory PDF</h2>
-	<!--<a href="#" onclick="openRequestedPopup(); return false;">
-		<img src="/sites/default/files/images/help.gif" alt="" style="border:none;" width="15px" height="15px" />
-	</a>
-	-->
+
 	<div class="search-item">
-		<a href="/content/uploads/modules/directory/OCDLA_2021_Membership_Directory.pdf">Download</a>
+		<a href="/content/uploads/modules/directory/OCDLA_2022_Membership_Directory.pdf">Download</a>
 	</div>
-	<div class="search-item">
-           — also includes these sections, updated each September: <br>
-        • Public Defender Boards <br>
-        • Office of Public Defense Services Staff<br>
-        • Federal Public Defender Offices<br>
-  </div>
+	<ul>
+        <li>— also includes these sections, updated each September:</li>
+        <li>Public Defender Boards</li>
+        <li>Office of Public Defense Services Staff</li>
+        <li>Federal Public Defender Offices</li>
+	</ul>
+
 </div>
 
 
 <?php if(false && $feature["alpha-search"]): ?>
-<div id="search-box-alpha" class="search-box">
-	<h2>Browse by Last Name</h2>
-	<a href="/directory/search?LastName=A&amp;op=STARTS%20WITH">A</a>&nbsp; 
-	<a href="/directory/search?LastName=B&amp;op=STARTS%20WITH">B</a>&nbsp; 
-	<a href="/directory/search?LastName=C&amp;op=STARTS%20WITH">C</a>&nbsp; 
-	<a href="/directory/search?LastName=D&amp;op=STARTS%20WITH">D</a>&nbsp; 
-	<a href="/directory/search?LastName=E&amp;op=STARTS%20WITH">E</a>&nbsp; 
-	<a href="/directory/search?LastName=F&amp;op=STARTS%20WITH">F</a>&nbsp;  
-	<a href="/directory/search?LastName=G&amp;op=STARTS%20WITH">G</a>&nbsp; 
-	<a href="/directory/search?LastName=H&amp;op=STARTS%20WITH">H</a>&nbsp; 
-	<a href="/directory/search?LastName=I&amp;op=STARTS%20WITH">I</a>&nbsp; 
-	<a href="/directory/search?LastName=J&amp;op=STARTS%20WITH">J</a>&nbsp; 
-	<a href="/directory/search?LastName=K&amp;op=STARTS%20WITH">K</a>&nbsp; 
-	<a href="/directory/search?LastName=L&amp;op=STARTS%20WITH">L</a>&nbsp; 
-	<a href="/directory/search?LastName=M&amp;op=STARTS%20WITH">M</a>&nbsp; 
-	<a href="/directory/search?LastName=N&amp;op=STARTS%20WITH">N</a>&nbsp; 
-	<a href="/directory/search?LastName=O&amp;op=STARTS%20WITH">O</a>&nbsp; 
-	<a href="/directory/search?LastName=P&amp;op=STARTS%20WITH">P</a>&nbsp; 
-	<a href="/directory/search?LastName=Q&amp;op=STARTS%20WITH">Q</a>&nbsp; 
-	<a href="/directory/search?LastName=R&amp;op=STARTS%20WITH">R</a>&nbsp; 
-	<a href="/directory/search?LastName=S&amp;op=STARTS%20WITH">S</a>&nbsp; 
-	<a href="/directory/search?LastName=T&amp;op=STARTS%20WITH">T</a>&nbsp; 
-	<a href="/directory/search?LastName=U&amp;op=STARTS%20WITH">U</a>&nbsp; 
-	<a href="/directory/search?LastName=V&amp;op=STARTS%20WITH">V</a>&nbsp; 
-	<a href="/directory/search?LastName=W&amp;op=STARTS%20WITH">W</a>&nbsp; 
-	<a href="/directory/search?LastName=X&amp;op=STARTS%20WITH">X</a>&nbsp; 
-	<a href="/directory/search?LastName=Y&amp;op=STARTS%20WITH">Y</a>&nbsp; 
-	<a href="/directory/search?LastName=Z&amp;op=STARTS%20WITH">Z</a>&nbsp; 
-</div>
+	<div id="search-box-alpha" class="search-box">
+		<h2>Browse by Last Name</h2>
+		<a href="/directory/search?LastName=A&amp;op=STARTS%20WITH">A</a>&nbsp; 
+		<a href="/directory/search?LastName=B&amp;op=STARTS%20WITH">B</a>&nbsp; 
+		<a href="/directory/search?LastName=C&amp;op=STARTS%20WITH">C</a>&nbsp; 
+		<a href="/directory/search?LastName=D&amp;op=STARTS%20WITH">D</a>&nbsp; 
+		<a href="/directory/search?LastName=E&amp;op=STARTS%20WITH">E</a>&nbsp; 
+		<a href="/directory/search?LastName=F&amp;op=STARTS%20WITH">F</a>&nbsp;  
+		<a href="/directory/search?LastName=G&amp;op=STARTS%20WITH">G</a>&nbsp; 
+		<a href="/directory/search?LastName=H&amp;op=STARTS%20WITH">H</a>&nbsp; 
+		<a href="/directory/search?LastName=I&amp;op=STARTS%20WITH">I</a>&nbsp; 
+		<a href="/directory/search?LastName=J&amp;op=STARTS%20WITH">J</a>&nbsp; 
+		<a href="/directory/search?LastName=K&amp;op=STARTS%20WITH">K</a>&nbsp; 
+		<a href="/directory/search?LastName=L&amp;op=STARTS%20WITH">L</a>&nbsp; 
+		<a href="/directory/search?LastName=M&amp;op=STARTS%20WITH">M</a>&nbsp; 
+		<a href="/directory/search?LastName=N&amp;op=STARTS%20WITH">N</a>&nbsp; 
+		<a href="/directory/search?LastName=O&amp;op=STARTS%20WITH">O</a>&nbsp; 
+		<a href="/directory/search?LastName=P&amp;op=STARTS%20WITH">P</a>&nbsp; 
+		<a href="/directory/search?LastName=Q&amp;op=STARTS%20WITH">Q</a>&nbsp; 
+		<a href="/directory/search?LastName=R&amp;op=STARTS%20WITH">R</a>&nbsp; 
+		<a href="/directory/search?LastName=S&amp;op=STARTS%20WITH">S</a>&nbsp; 
+		<a href="/directory/search?LastName=T&amp;op=STARTS%20WITH">T</a>&nbsp; 
+		<a href="/directory/search?LastName=U&amp;op=STARTS%20WITH">U</a>&nbsp; 
+		<a href="/directory/search?LastName=V&amp;op=STARTS%20WITH">V</a>&nbsp; 
+		<a href="/directory/search?LastName=W&amp;op=STARTS%20WITH">W</a>&nbsp; 
+		<a href="/directory/search?LastName=X&amp;op=STARTS%20WITH">X</a>&nbsp; 
+		<a href="/directory/search?LastName=Y&amp;op=STARTS%20WITH">Y</a>&nbsp; 
+		<a href="/directory/search?LastName=Z&amp;op=STARTS%20WITH">Z</a>&nbsp; 
+	</div>
 <?php endif; ?>
