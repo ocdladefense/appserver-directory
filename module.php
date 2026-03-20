1504010ada01b6d8124d640560e3a19ddfdf716a
Ubuntu <ubuntu@ip-172-31-58-171.us-west-2.compute.internal>
2026-03-20 11:24:29 -0700

    Dispaly primary fields.

--- module.php
+++ module.php
@@ -391,6 +391,7 @@ class DirectoryModule extends Module {
         $metadata = $api->getSobjectMetadata("Contact");
         $sobject = SObject::fromMetadata($metadata);
         $primaryFields = $sobject->getPicklist("Ocdla_Expert_Witness_Primary__c");
+        var_dump($primaryFields);exit;
 
 
         if(!isset($experts) || count($experts) < 1) {
